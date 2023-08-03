752e3e67e8cff1e48f3e86f70417c56921d29904
ZenwayProjects <zenway15@gmail.com>
2023-08-03 09:17:11 -0500

    Controladores de subida y bajada de archivos

---
+++ src/main/java/com/example/filestoragesystem/FileStorageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.filestoragesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStorageSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileStorageSystemApplication.class, args);
+    }
+
+}
